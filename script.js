c92505b5008fb42bae5ec2ff8ea356035eb261fc
Buff4eyes <68614831+cmokbel1@users.noreply.github.com>
2021-11-22 13:17:00 -0800

    randomWorkout Button

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-// API search for excercise //
-  // added 11/19/2021
-axios.get(`https://exercisedb.p.rapidapi.com/exercises?rapidapi-key=8d36f60e47msha974aed1faa2b08p16ca05jsna91e6d65d953`)
-        .then(res => {
-          const exercise = res.data
-          let dataLog = console.log(exercise)
-          console.log(exercise[0]);
 
-          
-            document.getElementById('workoutHTML').innerHTML = `
-            <h1>${exercise[0].bodyPart}</h1>
-            <img src="${exercise[0].gifUrl}" alt="">
-            `
-    })
-    .catch(err => console.log(err))
-
-  })
-
-
-
- 
 
 const options = {
   method: 'GET',
   url: 'https://shazam-core.p.rapidapi.com/v1/charts/genre-country',
-  params: {country_code: 'US', genre_code: 'POP', limit: '50'},
+  params: {country_code: 'US', genre_code: `${userChoice}`', limit: '50'},
   headers: {
     'x-rapidapi-host': 'shazam-core.p.rapidapi.com',
     'x-rapidapi-key': '8d36f60e47msha974aed1faa2b08p16ca05jsna91e6d65d953'
@@ -48,7 +28,7 @@ axios.request(options).then(function (music) {
 
 // SEARCH BUTTON FUNCTIONS //
     // added 11/22/2021//
-    document.getElementById('randomWorkout').onabort('click', () => {
+    document.getElementById('randomWorkout').on('click', () => {
       // API search for excercise //
       // added 11/19/2021
       axios.get(`https://exercisedb.p.rapidapi.com/exercises?rapidapi-key=8d36f60e47msha974aed1faa2b08p16ca05jsna91e6d65d953`)
