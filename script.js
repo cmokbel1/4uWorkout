b05186f9a6f7c04e3908408563f0cb1a4bbdd97f
Buff4eyes <68614831+cmokbel1@users.noreply.github.com>
2021-11-22 11:48:47 -0800

    random workout on click

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// API search for excercise //
+  // added 11/19/2021
 axios.get(`https://exercisedb.p.rapidapi.com/exercises?rapidapi-key=8d36f60e47msha974aed1faa2b08p16ca05jsna91e6d65d953`)
         .then(res => {
           const exercise = res.data
@@ -15,7 +17,7 @@ axios.get(`https://exercisedb.p.rapidapi.com/exercises?rapidapi-key=8d36f60e47ms
         .catch(err => console.log(err))
 
 
-
+ 
 
 const options = {
   method: 'GET',
@@ -43,7 +45,31 @@ axios.request(options).then(function (music) {
 
 //------------------------------------------------------------------------------------------------ ABOVE IS API FUNCTIONS, NO NEED TO EDIT ----
 
+// SEARCH BUTTON FUNCTIONS //
+    // added 11/22/2021//
+    document.getElementById('randomWorkout').onabort('click', () => {
+      // API search for excercise //
+      // added 11/19/2021
+      axios.get(`https://exercisedb.p.rapidapi.com/exercises?rapidapi-key=8d36f60e47msha974aed1faa2b08p16ca05jsna91e6d65d953`)
+        .then(res => {
+          const exercise = res.data
+          let dataLog = console.log(exercise)
+          console.log(exercise[0]);
+          // select random excercise from muscle group //
+          let randomWorkout = Math.floor(Math.random() * specificArr.length)
+          // edit HTML to match //
+          document.getElementById('workoutHTML').innerHTML = `
+            <h1>${exercise[randomWorkout].bodyPart}</h1>
+            <img src="${exercise[randomWorkout].gifUrl}" alt="">
+            `
+            // splice workout from array once used //
+          specificArr.splice('randomWorkout',1)
 
+        })
+        .catch(err => console.log(err))
+
+
+    })
 
 // document.getElementById("workoutHTML").innerHTML = `
 // <h1>qwd</h1>
